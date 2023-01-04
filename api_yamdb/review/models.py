d955bf88f76659cf7bd93921cecab8c6c4af8ae5
Станислав Андрющенко <StAndryushchenko@yandex.ru>
2023-01-04 22:58:08 +0300

    Добавил модель Review

--- api_yamdb/review/models.py
+++ api_yamdb/review/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class User(AbstractUser):
@@ -25,3 +26,32 @@ class User(AbstractUser):
     role = models.CharField(choices=roles,
                             default='user',
                             max_length=50)
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        verbose_name='Значение оценки',
+        validators=[
+            MaxValueValidator(10, message='Введите значение от 1 до 10'),
+            MinValueValidator(1, message='Введите значение от 1 до 10')
+        ]
+    )
+    text = models.TextField(
+        verbose_name='Текст отзыва',
+        max_length=300
+    )
+    created = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
