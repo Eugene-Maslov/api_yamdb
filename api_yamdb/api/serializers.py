6424c46e5a46ad33d0fc0f76722776bc03ee91da
Евгений Маслов <Eugene.Maslov@yandex.ru>
2023-01-10 11:51:10 +0300

    Final version for the first review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -70,8 +70,7 @@ class UserSerializer(serializers.ModelSerializer):
                                    required=True)
 
     class Meta:
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio',
-                  'role')
+        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
         model = User
 
     def validate(self, data):
